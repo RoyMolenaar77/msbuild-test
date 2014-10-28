53fbab66343fcccff5f1b3fdcf44f8ace03872eb
Roy Molenaar <r.molenaar@diract-it.nl>
2014-10-28 22:05:07 +0100

    typo resolved + create script added.

---
+++ CreateRevisions.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE Revisions
+ (
+	ReleaseID int IDENTITY(1,1) PRIMARY KEY,
+	ReleaseDate date NOT NULL,
+	major int NOT NULL,
+	minor int NOT NULL,
+	version int NOT NULL,
+	revision int NOT NULL,
+ );
