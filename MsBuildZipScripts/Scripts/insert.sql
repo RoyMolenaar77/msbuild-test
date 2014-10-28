bbc3f6f8bdd73f43d9a954a98cb52c1dd23c4b8c
Roy Molenaar <r.molenaar@diract-it.nl>
2014-10-28 21:54:17 +0100

    modified insert query

--- MsBuildZipScripts/Scripts/insert.sql
+++ MsBuildZipScripts/Scripts/insert.sql
@@ -1,3 +1,3 @@
 Begin
-Insert into dbo.Revisions(major,minor,version,revision) values(@P1,@P2,@P3,@P4);
+Insert into dbo.Revisions(ReleaseDate,major,minor,version,revision) values(GETDATE(),@P1,@P2,@P3,@P4);
 End;
